bc97e9c60b5e3cdac62b7c373e59f979f3d5c028
Lorgio Trinidad <lorgio.trinidad@globant.com>
2019-08-20 01:12:36 -0400

    Add base web socket server

---
+++ server.js
@@ -0,0 +1,26 @@
+var app = require('express')();
+const uuidv1 = require('uuid/v1');
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected:', socket.id);
+  console.log('handshake:', socket.handshake)
+
+  socket.on('getClientId', function(app, fn){
+    console.log('App: ' + app);
+    fn(uuidv1());
+  });
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected', socket.id);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
